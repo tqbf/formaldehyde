3e21e1ba21825599ca43921af0a7b364976214ed
Thomas H. Ptacek <thomas@RIDGELAND.local>
2012-09-26 19:22:57 -0500

    one process, multiple plugs

--- src/matasano/blackbag/replug/main.go
+++ src/matasano/blackbag/replug/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"matasano/blackbag"
 	"os"
-	"strings"
 	"regexp"
 	"log"
 	"strconv"
@@ -14,26 +13,32 @@ var (
 )
 
 func main() {
-	rest := strings.Join(os.Args[1:], " ")
-	
-	m := spec_exp.FindSubmatch([]byte(rest))
-	if m == nil { 
-		log.Fatal("replug target[:port[@lport]]")		
-	} 
+	var wait chan bool
 
-	host := string(m[1])
-	port, _ := strconv.ParseInt(string(m[2]), 0, 32)
-	lport := port
-	if len(m) > 2 {
-		lport, _ = strconv.ParseInt(string(m[3]), 0, 32)
-	}
-	
-	r, e := blackbag.NewReplug(host, int(port), int(lport))
-	if e != nil {
-		log.Fatal("Bind: ", e)
+	for i := 1; i < len(os.Args); i++ {
+		rest := os.Args[i]
+
+	 	m := spec_exp.FindSubmatch([]byte(rest))
+	 	if m == nil { 
+	 		log.Fatal("replug target[:port[@lport]]")		
+	 	} 
+	 
+	 	host := string(m[1])
+	 	port, _ := strconv.ParseInt(string(m[2]), 0, 32)
+	 	lport := port
+	 	if len(m) > 2 {
+	 		lport, _ = strconv.ParseInt(string(m[3]), 0, 32)
+	 	}
+	 	
+	 	r, e := blackbag.NewReplug(host, int(port), int(lport))
+	 	if e != nil {
+	 		log.Fatal("Bind: ", e)
+	 	}
+	 
+		wait = r.Wait
+
+	 	go r.Loop()
 	}
 
-	go r.Loop()
-	
-	<- r.Wait
+	<- wait	
 }
